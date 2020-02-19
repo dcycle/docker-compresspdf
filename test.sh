f041467da2baaa922feb23f7a5a755bf612d16af
alberto56 <albert@mediatribe.net>
2020-02-19 16:40:04 -0500

    do not attempt to pull non-existing image

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 set -e
 docker pull python
-docker pull dcycle/compresspdf:1
+# docker pull dcycle/compresspdf:1
 docker build -t local-dcycle-compresspdf-image .
 
 docker run --rm -v $(pwd)/example01/test:/app local-dcycle-compresspdf-image screen generic_pdf3.pdf generic_pdf3.compressed.pdf
